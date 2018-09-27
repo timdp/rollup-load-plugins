9172ea8fdc257392abbc9968ace1731ae2cbd819
Tim De Pauw <timdp@users.noreply.github.com>
2018-09-27 09:58:03 +0200

    Modernize

--- index.js
+++ index.js
@@ -1,1 +1,88 @@
-module.exports = require('./lib/').default
+const camelCase = require('camelcase')
+const mem = require('mem')
+const micromatch = require('micromatch')
+const readPkgUp = require('read-pkg-up')
+const resolve = require('resolve')
+const { dirname } = require('path')
+
+const defaultOptions = {
+  pattern: ['rollup-plugin-*'],
+  scope: ['dependencies', 'devDependencies', 'peerDependencies'],
+  replaceString: /^rollup-plugin-/,
+  camelize: true
+}
+
+const parseDependency = name => {
+  const idx = name.indexOf('/')
+  return {
+    name,
+    scope: idx < 0 ? null : name.substring(1, idx),
+    id: idx < 0 ? name : name.substring(idx + 1)
+  }
+}
+
+const getDependencies = (pkg, scope) => {
+  return Array.prototype.concat
+    .apply(
+      [],
+      scope.map(key => {
+        return Object.keys(pkg[key] || {})
+      })
+    )
+    .map(parseDependency)
+}
+
+const filterDependencies = (pkg, { scope, pattern }) =>
+  getDependencies(pkg, scope).filter(
+    ({ id }) => micromatch(id, pattern).length > 0
+  )
+
+const getKey = (plugin, { replaceString, camelize }) => {
+  const replaced = plugin.replace(replaceString, '')
+  return camelize ? camelCase(replaced) : replaced
+}
+
+const allocateParent = (result, scope) => {
+  if (scope != null) {
+    if (result[scope] == null) {
+      result[scope] = {}
+    }
+    return result[scope]
+  } else {
+    return result
+  }
+}
+
+const defineProperty = (result, key, plugin, resolveOptions) => {
+  Object.defineProperty(result, key, {
+    get: mem(() => require(resolve.sync(plugin, resolveOptions))),
+    enumerable: true
+  })
+}
+
+const buildResult = (plugins, { config, replaceString, camelize }) => {
+  const resolveOptions = { basedir: dirname(config) }
+  const result = {}
+  for (let i = 0; i < plugins.length; ++i) {
+    const { name, scope, id } = plugins[i]
+    const parent = allocateParent(result, scope)
+    const key = getKey(id, { replaceString, camelize })
+    defineProperty(parent, key, name, resolveOptions)
+  }
+  return result
+}
+
+const loadPlugins = (options = {}) => {
+  const { pattern, scope, replaceString, camelize, cwd } = Object.assign(
+    {},
+    defaultOptions,
+    options
+  )
+  const { pkg, path: config } = readPkgUp.sync({ cwd })
+  const plugins = filterDependencies(pkg, { scope, pattern })
+  return buildResult(plugins, { config, replaceString, camelize })
+}
+
+delete require.cache[__filename]
+
+module.exports = loadPlugins
---
+++ test.js
@@ -0,0 +1,135 @@
+const { expect } = require('chai')
+const path = require('path')
+const loadPlugins = require('./')
+
+const load = function (name, options = {}) {
+  const oldCwd = process.cwd()
+  if (options != null && options.cwd == null) {
+    const dir = path.join(__dirname, 'test/fixtures', name)
+    process.chdir(dir)
+  }
+  try {
+    return options === false ? loadPlugins() : loadPlugins(options)
+  } catch (err) {
+    throw err
+  } finally {
+    if (options != null && options.cwd == null) {
+      process.chdir(oldCwd)
+    }
+  }
+}
+
+describe('basic use', function () {
+  it('loads the plugin from dependencies', function () {
+    const p = load('basic-deps')
+    expect(p).to.have.all.keys('foo')
+  })
+
+  it('loads the plugins from dependencies and devDependencies', function () {
+    const p = load('basic-dev-deps')
+    expect(p).to.have.all.keys('foo', 'bar')
+  })
+
+  it('lazy-loads the plugin', function () {
+    const p = load('basic-deps')
+    expect(p.foo).to.deep.equal({ name: 'foo' })
+  })
+})
+
+describe('multiple plugins', function () {
+  it('loads every plugin', function () {
+    const p = load('multiple')
+    expect(p).to.contain.all.keys('foo', 'bar')
+  })
+
+  it('does not load other dependencies', function () {
+    const p = load('multiple')
+    expect(p).to.have.all.keys('foo', 'bar')
+  })
+})
+
+describe('scopes', function () {
+  it('turns a scope into a nested object', function () {
+    const p = load('scope')
+    expect(p.scope).to.be.an('object')
+  })
+
+  it('does not load other scoped dependencies', function () {
+    const p = load('scope')
+    expect(p.scope).to.have.all.keys('foo', 'bar')
+  })
+
+  it('mixes scoped and unscoped plugins')
+})
+
+describe('options object', function () {
+  it('can be omitted', function () {
+    expect(function () {
+      load('basic-deps', false)
+    }).to.not.throw(Error)
+  })
+
+  it('can be null', function () {
+    expect(function () {
+      load('basic-deps', null)
+    }).to.not.throw(Error)
+  })
+})
+
+describe('pattern option', function () {
+  it('applies the custom pattern', function () {
+    const p = load('multiple', { pattern: '*' })
+    expect(p).to.have.all.keys('foo', 'bar', 'baz')
+  })
+})
+
+describe('scope option', function () {
+  it('loads the plugins from the scope', function () {
+    const p = load('basic-dev-deps', { scope: ['dependencies'] })
+    expect(p).to.have.all.keys('foo')
+    expect(p).to.not.have.all.keys('bar')
+  })
+})
+
+describe('replaceString option', function () {
+  it('replaces a string', function () {
+    const p = load('basic-deps', { replaceString: '-plugin' })
+    expect(p).to.have.all.keys('rollupFoo')
+  })
+
+  it('replaces a regexp', function () {
+    const p = load('basic-deps', { replaceString: /l+/g })
+    expect(p).to.have.all.keys('roupPuginFoo')
+  })
+})
+
+describe('camelize option', function () {
+  it('camelizes the plugin name', function () {
+    const p = load('camelize')
+    expect(p).to.contain.all.keys('fooBar')
+  })
+
+  it('does not camelize the plugin name if disabled', function () {
+    const p = load('camelize', { camelize: false })
+    expect(p).to.contain.all.keys('foo-bar')
+  })
+
+  it('camelizes the scoped plugin name', function () {
+    const p = load('scope-camelize')
+    expect(p.scope).to.have.all.keys('fooBar')
+  })
+
+  it('does not camelize the scoped plugin name if option disabled', function () {
+    const p = load('scope-camelize', { camelize: false })
+    expect(p.scope).to.have.all.keys('foo-bar')
+  })
+})
+
+describe('cwd option', function () {
+  it('overrides cwd', function () {
+    const p = load('basic-deps', {
+      cwd: path.join(__dirname, 'test/fixtures/basic-deps')
+    })
+    expect(p).to.have.all.keys('foo')
+  })
+})
